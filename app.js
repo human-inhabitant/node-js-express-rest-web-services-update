ed0c2dc3567f819112e8a55d2f536a46c18ae8ae
Human Inhabitant <human-inhabitant@pm.me>
2020-10-01 16:27:43 -0400

    Completed module 06 and m07...

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost:27017/bookAPI', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+if (process.env.ENV === 'test') {
+  console.info('Test DB');
+  const db = mongoose.connect('mongodb://localhost:27017/bookAPI-test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+} else {
+  console.info('Production DB');
+  const db = mongoose.connect('mongodb://localhost:27017/bookAPI', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+}
+
 const port = process.env.PORT || 3e3;
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
@@ -17,7 +27,9 @@ app.use('/api', bookRouter);
 app.get('/', (req, res) => {
   res.send('Welcome to my API!');
 });
-app.listen(port, () => {
+app.server = app.listen(port, () => {
   console.info('Start: %s', new Date());
   console.info('Listening on port: %d', port);
 });
+
+module.exports = app;
